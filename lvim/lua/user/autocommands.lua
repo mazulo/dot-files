78317f307de20cfa3c9e4e11ddff9b63c44fc1e9
Patrick Mazulo <mazulo@d7.dev>
2024-02-27 11:04:36 -0300

    fix: add autocommand to reset folding after file reload

--- lvim/lua/user/autocommands.lua
+++ lvim/lua/user/autocommands.lua
@@ -1,35 +1,40 @@
 -- Autocommands (`:help autocmd`) <https://neovim.io/doc/user/autocmd.html>
 lvim.autocommands = {
-  {
-    { "BufEnter", "BufWinEnter" },
     {
-        group = "lvim_user",
-        pattern = "Makefile",
-        command = "setlocal shiftwidth=2 tabstop=2 noexpandtab",
+        { "BufEnter", "BufWinEnter" },
+        {
+            group = "lvim_user",
+            pattern = "Makefile",
+            command = "setlocal shiftwidth=2 tabstop=2 noexpandtab",
+        },
+        {
+            group = "lvim_user",
+            pattern = "justfile",
+            command = "setlocal shiftwidth=2 tabstop=2 noexpandtab",
+        },
     },
     {
-        group = "lvim_user",
-        pattern = "justfile",
-        command = "setlocal shiftwidth=2 tabstop=2 noexpandtab",
+        { "FileType", },
+        {
+            group = "lvim_user",
+            pattern = "zsh",
+            callback = function()
+            -- let treesitter use bash highlight for zsh files as well
+                require("nvim-treesitter.highlight").attach(0, "bash")
+            end,
+        },
     },
-  },
-  {
-    { "FileType", },
-    {
-        group = "lvim_user",
-        pattern = "zsh",
-        callback = function()
-        -- let treesitter use bash highlight for zsh files as well
-            require("nvim-treesitter.highlight").attach(0, "bash")
-        end,
-    },
-  },
 }
-  
+
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = "zsh",
-  callback = function()
+    pattern = "zsh",
+    callback = function()
     -- let treesitter use bash highlight for zsh files as well
     require("nvim-treesitter.highlight").attach(0, "bash")
-  end,
+    end,
+})
+
+vim.api.nvim_create_autocmd({ "FileChangedShellPost" }, {
+    pattern = { "*" },
+    command = "e %",
 })
