c7ffd1f0e5d5da89546a0636dbe019802a3ff41c
Patrick Mazulo <mazulo@d7.dev>
2024-04-15 11:49:43 -0300

    feat: include keymaps for trouble

--- .config/lvim/lua/user/keymaps.lua
+++ .config/lvim/lua/user/keymaps.lua
@@ -47,3 +47,12 @@ vim.cmd("nnoremap <silent> ]d :lua vim.lsp.diagnostic.goto_next({popup_opts = {b
 
 -- triptych
 vim.keymap.set('n', '<Leader>-', ':Triptych<CR>', { silent = true })
+
+-- trouble
+vim.keymap.set("n", "<leader>xx", function() require("trouble").toggle() end)
+vim.keymap.set("n", "<leader>xw", function() require("trouble").toggle("workspace_diagnostics") end)
+vim.keymap.set("n", "<leader>xd", function() require("trouble").toggle("document_diagnostics") end)
+vim.keymap.set("n", "<leader>xq", function() require("trouble").toggle("quickfix") end)
+vim.keymap.set("n", "<leader>xl", function() require("trouble").toggle("loclist") end)
+vim.keymap.set("n", "gR", function() require("trouble").toggle("lsp_references") end)
+
