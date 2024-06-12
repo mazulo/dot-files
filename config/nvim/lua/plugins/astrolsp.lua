01a7ae435df0c28592614c6190e99ffd0ed6c514
Patrick Mazulo <pmazulo@gmail.com>
2024-06-12 16:48:08 -0300

    feat: add lsp_codelens_refresh

--- config/nvim/lua/plugins/astrolsp.lua
+++ config/nvim/lua/plugins/astrolsp.lua
@@ -93,6 +93,16 @@ return {
           callback = function() vim.lsp.buf.clear_references() end,
         },
       },
+      lsp_codelens_refresh = {
+        cond = "textDocument/codeLens",
+        {
+          event = { "InsertLeave", "BufEnter" },
+          desc = "Refresh codelens (buffer)",
+          callback = function(args)
+            if require("astrolsp").config.features.codelens then vim.lsp.codelens.refresh { bufnr = args.buf } end
+          end,
+        },
+      },
     },
     mappings = {
       n = {
