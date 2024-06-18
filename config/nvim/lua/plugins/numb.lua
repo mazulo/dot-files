c28827772ce09ad5df2d9bfd08fdefef27c20795
Patrick Mazulo <pmazulo@gmail.com>
2024-06-18 18:59:33 -0300

    feat: add numb plugin

---
+++ config/nvim/lua/plugins/numb.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nacro90/numb.nvim",
+    config = function()
+      require("numb").setup {
+        show_numbers = true,
+        show_cursorline = true,
+        hide_relativenumbers = true,
+        number_only = false,
+        centered_peeking = true,
+      }
+    end,
+  },
+}
