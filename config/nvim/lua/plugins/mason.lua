e8c1892e718e793dd6a6aa07e7c9fa142630bd07
Patrick Mazulo <mazulo@d7.dev>
2024-06-18 13:05:29 -0300

    feat: add cmake to list of ensured installed LSPs

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,5 +1,3 @@
--- Customize Mason plugins
-
 ---@type LazySpec
 return {
   -- use mason-lspconfig to configure LSP installations
@@ -10,6 +8,7 @@ return {
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
         "lua_ls",
+        "cmake",
         -- add more arguments for adding more language servers
       })
     end,
