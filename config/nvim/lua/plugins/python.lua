05582fb9f2351e569cc868f4a8c0a3938bdbe196
Patrick Mazulo <mazulo@d7.dev>
2024-08-27 13:23:38 -0300

    remove instalation of mypy

--- config/nvim/lua/plugins/python.lua
+++ config/nvim/lua/plugins/python.lua
@@ -54,6 +54,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     optional = true,
     opts = function(_, opts)
+      -- opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "mypy" })
       opts.ensure_installed = vim.tbl_filter(
         function(v) return not vim.tbl_contains({ "black", "isort" }, v) end,
         opts.ensure_installed
@@ -79,6 +80,7 @@ return {
         function(v) return not vim.tbl_contains({ "black", "isort" }, v) end,
         opts.ensure_installed
       )
+      opts.auto_update = true
     end,
   },
   {
