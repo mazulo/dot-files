de0a1016238a53955365ae04a3291ad58b40e687
Patrick Mazulo <pmazulo@gmail.com>
2025-05-20 14:00:20 -0300

    feat: first try on writing a new plugin to use windsurf

---
+++ config/nvim/lua/plugins/codeium.lua
@@ -0,0 +1,91 @@
+return {
+  "Exafunction/windsurf.nvim",
+  event = "User AstroFile",
+  cmd = "Codeium",
+  config = function()
+    local is_cmp_available = require("astrocore").is_available
+    local is_cmp_enabled = is_cmp_available "nvim-cmp" or is_cmp_available "blink.cmp"
+    require("codeium").setup {
+      enable_chat = true,
+      enable_cmp_source = is_cmp_enabled,
+      virtual_text = {
+        enabled = not is_cmp_enabled,
+      },
+    }
+  end,
+  dependencies = {
+    {
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = {
+        icons = {
+          Codeium = "",
+        },
+      },
+    },
+    {
+      "AstroNvim/astrocore",
+      ---@param opts AstroCoreOpts
+      opts = function(_, opts)
+        return require("astrocore").extend_tbl(opts, {
+          mappings = {
+            n = {
+              ["<Leader>;"] = {
+                desc = require("astroui").get_icon("Codeium", 1, true) .. "Codeium",
+              },
+              ["<Leader>;o"] = {
+                desc = "Open Chat",
+                function() vim.cmd "Codeium Chat" end,
+              },
+            },
+          },
+        })
+      end,
+    },
+  },
+  specs = {
+    {
+      "hrsh7th/nvim-cmp",
+      optional = true,
+      opts = function(_, opts)
+        -- Inject codeium into cmp sources, with high priority
+        table.insert(opts.sources, 1, {
+          name = "codeium",
+          group_index = 1,
+          priority = 10000,
+        })
+      end,
+    },
+    {
+      "saghen/blink.cmp",
+      specs = { "Saghen/blink.compat", version = "*", lazy = true, opts = {} },
+      opts = {
+        sources = {
+          default = { "lsp", "path", "snippets", "buffer", "codeium" },
+          providers = {
+            codeium = { name = "Codeium", module = "codeium.blink", async = true },
+          },
+        },
+      },
+    },
+    {
+      "onsails/lspkind.nvim",
+      optional = true,
+      -- Adds icon for codeium using lspkind
+      opts = function(_, opts)
+        if not opts.symbol_map then opts.symbol_map = {} end
+        opts.symbol_map.Codeium = require("astroui").get_icon("Codeium", 1, true)
+      end,
+    },
+    {
+      "echasnovski/mini.icons",
+      optional = true,
+      -- Adds icon for codeium using mini.icons
+      opts = function(_, opts)
+        if not opts.lsp then opts.lsp = {} end
+        if not opts.symbol_map then opts.symbol_map = {} end
+        opts.symbol_map.codeium = { glyph = require("astroui").get_icon("Codeium", 1, true), hl = "MiniIconsCyan" }
+      end,
+    },
+  },
+}
--- config/nvim/lua/plugins/mappings.lua
+++ config/nvim/lua/plugins/mappings.lua
@@ -18,6 +18,35 @@ return {
           },
           ["<C-s>"] = { ":w!<cr>", desc = "Save changes to the file in the current buffer" }, -- change description but the same command
           -- LSP mappings
+          gd = {
+            function()
+              require("snacks").picker.lsp_definitions {
+                finder = "lsp_definitions",
+                format = "file",
+                include_current = false,
+                auto_confirm = true,
+                confirm = "edit_vsplit",
+                jump = { tagstack = true, reuse_win = false },
+              }
+            end,
+            desc = "Goto Definition",
+          },
+          gD = {
+            function()
+              require("snacks").picker.lsp_declarations {
+                finder = "lsp_declarations",
+                format = "file",
+                include_current = false,
+                auto_confirm = true,
+                confirm = "edit_vsplit",
+                jump = { tagstack = true, reuse_win = true },
+              }
+            end,
+            desc = "Goto Declaration",
+          },
+          ["gr"] = { function() require("snacks").picker.lsp_references() end, nowait = true, desc = "References" },
+          ["gI"] = { function() require("snacks").picker.lsp_implementations() end, desc = "Goto Implementation" },
+          ["gy"] = { function() require("snacks").picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
           -- mappings related to the behavior of cutting/deleting/pasting
           x = { '"_x' },
           d = { '"_d' },
