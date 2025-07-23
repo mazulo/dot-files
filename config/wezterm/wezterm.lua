28d6f4776d7cdcc3dcdab4dc8125ebc2977dc169
Patrick Mazulo <pmazulo@gmail.com>
2025-07-23 10:03:26 -0300

    fix: update wezterm.lua with keybinds for neovim ctrl/ctrl+shift tab

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -3,42 +3,58 @@ local wezterm = require("wezterm")
 local config = {}
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
-config.color_scheme = "Afterglow"
+config.color_scheme = "Gogh (Gogh)"
 config.font = wezterm.font("Source Code Pro for Powerline", { weight = "Bold", italic = true })
 config.audible_bell = "Disabled"
-config.font_size = 10.5
+config.font_size = 11
 config.keys = {
-  {
-    key = "m",
-    mods = "CMD",
-    action = wezterm.action.DisableDefaultAssignment,
-  },
-  {
-    key = "Enter",
-    mods = "OPT",
-    action = wezterm.action.DisableDefaultAssignment,
-  },
-  { key = "Tab", mods = "CTRL",       action = wezterm.action.Nop },
-  { key = "Tab", mods = "SHIFT|CTRL", action = wezterm.action.Nop },
-  {
-    key = "Tab",
-    mods = "CTRL",
-    action = wezterm.action.SendKey({
-      key = "Tab",
-      mods = "OPT",
-    }),
-  },
-  {
-    key = "Tab",
-    mods = "SHIFT|CTRL",
-    action = wezterm.action.SendKey({
-      key = "Tab",
-      mods = "SHIFT",
-    }),
-  },
+	{
+		key = "m",
+		mods = "CMD",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{
+		key = "Enter",
+		mods = "OPT",
+		action = wezterm.action.DisableDefaultAssignment,
+	},
+	{ key = "Tab", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+	{ key = "Tab", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
+	-- { key = "RightArrow", mods = "CTRL", action = wezterm.action.DisableDefaultAssignment },
+	-- { key = "RightArrow", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
+	-- { key = "RightArrow", mods = "SHIFT|ALT|CTRL", action = wezterm.action.DisableDefaultAssignment },
+	{
+		key = "Tab",
+		mods = "CTRL",
+		action = wezterm.action.SendKey({
+			key = "Tab",
+			mods = "OPT",
+		}),
+	},
+	{
+		key = "Tab",
+		mods = "SHIFT|CTRL",
+		action = wezterm.action.SendKey({
+			key = "Tab",
+			mods = "SHIFT",
+		}),
+	},
+	-- {
+	-- 	key = "LeftArrow",
+	-- 	mods = "CTRL",
+	-- 	action = wezterm.action.SendKey({
+	-- 		key = "b",
+	-- 		mods = "CTRL",
+	-- 	}),
+	-- },
+	-- {
+	-- 	key = "RightArrow",
+	-- 	mods = "CTRL",
+	-- 	action = wezterm.action.SendKey({ key = "f", mods = "CTRL" }),
+	-- },
 }
 
 config.enable_tab_bar = false
